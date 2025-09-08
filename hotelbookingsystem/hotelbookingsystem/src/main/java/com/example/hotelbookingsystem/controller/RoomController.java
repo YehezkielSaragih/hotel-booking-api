30ab8c08fe2c93711bf76cf33f114322b52e31e8
Yehezkiel Stephanus Austin <yehezkiel.stephanus@gmail.com>
2025-09-08 11:12:09 +0700

    template fix

--- hotelbookingsystem/hotelbookingsystem/src/main/java/com/example/hotelbookingsystem/controller/RoomController.java
+++ hotelbookingsystem/hotelbookingsystem/src/main/java/com/example/hotelbookingsystem/controller/RoomController.java
@@ -1,4 +1,10 @@
 package com.example.hotelbookingsystem.controller;
 
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class RoomController {
+
+
 }
--- hotelbookingsystem/hotelbookingsystem/src/main/java/com/example/hotelbookingsystem/entity/Room.java
+++ hotelbookingsystem/hotelbookingsystem/src/main/java/com/example/hotelbookingsystem/entity/Room.java
@@ -1,4 +1,7 @@
 package com.example.hotelbookingsystem.entity;
 
+
 public class Room {
+
+
 }
