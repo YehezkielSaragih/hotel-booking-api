e9de9a9dd0c34ee5b28cb2c7abddf252068469f4
Yehezkiel Stephanus Austin <yehezkiel.stephanus@gmail.com>
2025-09-08 11:48:50 +0700

    fix controller

--- hotelbookingsystem/hotelbookingsystem/src/main/java/com/example/hotelbookingsystem/controller/BookingController.java
+++ hotelbookingsystem/hotelbookingsystem/src/main/java/com/example/hotelbookingsystem/controller/BookingController.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class BookingController {
 
     @Autowired
-    private Booking bookingRepo;
+    private BookingRepo bookingRepo;
 
     // POST /bookings buat booking
     @PostMapping
