8c02da499842b9e576311784db77dd2392c2edbc
Royzn <royzenanggatama@gmail.com>
2025-09-08 11:43:24 +0700

    creating entity

--- hotelbookingsystem/hotelbookingsystem/src/main/java/com/example/hotelbookingsystem/entity/Booking.java
+++ hotelbookingsystem/hotelbookingsystem/src/main/java/com/example/hotelbookingsystem/entity/Booking.java
@@ -1,4 +1,91 @@
 package com.example.hotelbookingsystem.entity;
 
+import java.util.*;
+
 public class Booking {
+
+    private int bookingId;
+    private String customerName;
+    private Date checkIn;
+    private Date checkOut;
+    private List<Room> roomList;
+
+    private Date createdAt;
+    private Date updatedAt;
+    private Date deletedAt;
+
+    public Booking(int bookingId, String customerName, Date checkIn, Date checkOut, List<Room> roomList, Date createdAt) {
+        this.bookingId = bookingId;
+        this.customerName = customerName;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+        this.roomList = roomList;
+        this.createdAt = createdAt;
+    }
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(Date checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(Date checkOut) {
+        this.checkOut = checkOut;
+    }
+
+    public List<Room> getRoomList() {
+        return roomList;
+    }
+
+    public void setRoomList(List<Room> roomList) {
+        this.roomList = roomList;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Date getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(Date deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+
+
 }
--- hotelbookingsystem/hotelbookingsystem/src/main/java/com/example/hotelbookingsystem/entity/Room.java
+++ hotelbookingsystem/hotelbookingsystem/src/main/java/com/example/hotelbookingsystem/entity/Room.java
@@ -1,7 +1,68 @@
 package com.example.hotelbookingsystem.entity;
-
+import java.util.Date;
+import java.util.List;
 
 public class Room {
 
+    private int roomId;
+    private boolean isAvailable;
+    private List<Booking> bookingList;
 
+    private Date createdAt;
+    private Date updatedAt;
+    private Date deletedAt;
+
+    public Room(int roomId, boolean isAvailable, Date createdAt) {
+        this.roomId = roomId;
+        this.isAvailable = isAvailable;
+        this.createdAt = createdAt;
+    }
+
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public List<Booking> getBookingList() {
+        return bookingList;
+    }
+
+    public void setBookingList(List<Booking> bookingList) {
+        this.bookingList = bookingList;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Date getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(Date deletedAt) {
+        this.deletedAt = deletedAt;
+    }
 }
